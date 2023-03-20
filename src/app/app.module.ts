871a8e02b1f558687e91895cd2e071eed90d44f6
Nenad Sredojević <n.sredojevic3@gmail.com>
2023-03-21 00:16:01 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminModule } from './modules/admin/admin.module';
+import { RegularUserModule } from './modules/regular-user/regular-user.module';
+import { GuestUserModule } from './modules/guest-user/guest-user.module';
+import { LoginRegistrationModule } from './modules/login-registration/login-registration.module';
+
+const routes: Routes = [
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes, { useHash: true}),
+    AdminModule,
+    RegularUserModule,
+    GuestUserModule,
+    LoginRegistrationModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
